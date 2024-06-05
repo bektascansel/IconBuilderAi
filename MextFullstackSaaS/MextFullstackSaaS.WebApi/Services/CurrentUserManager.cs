ed28588c459ba81131a976ae9dd347d31e736a82
bektascansel <cansel.bektas6447@gmail.com>
2024-06-05 17:43:08 +0300

    feat: Update CurrentUserManager

--- MextFullstackSaaS/MextFullstackSaaS.WebApi/Services/CurrentUserManager.cs
+++ MextFullstackSaaS/MextFullstackSaaS.WebApi/Services/CurrentUserManager.cs
@@ -14,7 +14,7 @@ namespace MextFullstackSaaS.WebApi.Services
         }
 
         public Guid UserId =>GetUserId();
-        //public Guid UserId => new Guid("35c16d2a-f25b-4701-9a74-ea1fb7ed6d93");
+      
         private Guid GetUserId()
         {
             var userId = _httpContextAccessor.HttpContext?.User?.FindFirstValue("uid");
