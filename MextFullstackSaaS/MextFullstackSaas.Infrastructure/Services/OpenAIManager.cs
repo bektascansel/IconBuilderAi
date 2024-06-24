3d1e01639495cd46e3a292a72f11a2969e9d5835
bektascansel <cansel.bektas6447@gmail.com>
2024-06-24 14:00:18 +0300

    [Feat]: Refactoring Code

--- MextFullstackSaaS/MextFullstackSaas.Infrastructure/Services/OpenAIManager.cs
+++ MextFullstackSaaS/MextFullstackSaas.Infrastructure/Services/OpenAIManager.cs
@@ -59,15 +59,15 @@ public class OpenAIManager : IOpenAIService
         var promptBuilder = new StringBuilder();
 
         promptBuilder.Append(
-            $"You're a World-class Icon Designer AI, Please generate an icon for a mobile app. Make sure the icon is fitting the full width and height. Generate icon with the following specifications below. I'll tip you 1000$ for your work, if I like it. ");
+            $"You're a World-class Icon Designer AI, Please generate an icon for a mobile app. Make sure the icon is fit the full width and height. Generate icon with the following specifications below. I'll tip you 1000$ for your work, if I like it.");
+
         promptBuilder.Append($"<DesignType>{request.DesignType}</DesignType>");
+
         promptBuilder.Append($"<Colour>{request.ColourCode}</Colour>");
+
         promptBuilder.Append($"<Shape>{request.Shape}</Shape>");
+
         promptBuilder.Append($"<Description>{request.Description}</Description>");
-
-        promptBuilder.Append(
-            $"Design Type: {request.DesignType}, Colour Code (Hex Code): {request.ColourCode}, Shape: {request.Shape}, Description:{request.Description} ");
-
 
         return promptBuilder.ToString();
     }
